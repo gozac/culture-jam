2ea5c6e6c2a0adad64b0fcc810167c09b6a3aeec
isaac <mrgozac@gmail.com>
2021-11-11 15:03:29 +0000

    add default level plateforme

---
+++ Assets/Script/resetPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetPlayer : MonoBehaviour
+{
+
+	public Transform resetPoint;
+
+    //Upon collision with another GameObject, this GameObject will reverse direction
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.transform.position = resetPoint.position;
+    }
+}
